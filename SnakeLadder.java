c8842b026e7a7422035652a7155bfbe0ae0f71ca
PoojithaKodali <72956653+PoojithaKodali@users.noreply.github.com>
2021-12-13 10:05:29 +0530

    Create SnakeLadder.java

---
+++ SnakeLadder.java
@@ -0,0 +1,16 @@
+package SnakeLadder;
+
+public class SnakeLadder {
+    public int start;
+    public int end;
+    public SnakeLadder(int start,int end){
+        this.start=start;
+        this.end=end;
+    }
+    public int getStart(){
+        return start;
+    }
+    public int getEnd(){
+        return end;
+    }
+}
